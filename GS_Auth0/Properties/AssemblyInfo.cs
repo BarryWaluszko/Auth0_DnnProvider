0f541dbd847d36631fdc8a5ecf20ed771c73731e
Barry <b.waluszko@hotmail.com>
2019-11-25 14:06:27 +0100

    New provider install package with updated license inside it.

--- GS_Auth0/Properties/AssemblyInfo.cs
+++ GS_Auth0/Properties/AssemblyInfo.cs
@@ -40,9 +40,9 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Revision and Build Numbers 
 // by using the '*' as shown below:
 
-[assembly: AssemblyVersion("00.00.01.*")]
+[assembly: AssemblyVersion("01.00.02.*")]
 [assembly: AssemblyDelaySign(false)]
 [assembly: AssemblyKeyFile("")]
 [assembly: AssemblyKeyName("")]
-[assembly: AssemblyFileVersion("00.00.01")]
+[assembly: AssemblyFileVersion("01.00.02")]
 
